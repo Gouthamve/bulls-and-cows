c4f8bbe7c85432c2600678aeadbe2c2dd110267d
Gouthamve <cs14btech11014@iith.ac.in>
2015-06-25 20:36:34 +0530

    Removed AI player
    Signed-off-by: Gouthamve <cs14btech11014@iith.ac.in>

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -16,27 +16,16 @@ function onHumanGameButtonClick() {
   player.addGuessEventListener(MainController.guess);
 }
 
-function onAIGameButtonClick() {
-  if (player != null) {
-    player.uninit();
-  }
-  player = new AI($(".aiGamePanel")[0]);
-  MainController.newGame();
-  LayoutManager.clearGuessList();
-  LayoutManager.showAIGamePanel();
-  player.addGuessEventListener(MainController.guess);
-  player.makeGuess();
-}
-
 function onHumanGuessButtonClick() {
   var event = new CustomEvent(
-	 "guess", 
-	 {
-	 	detail: {number: $(".humanGuessInput").val()},
+    "guess", {
+      detail: {
+        number: $(".humanGuessInput").val()
+      },
       bubbles: true,
       cancelable: true
     }
   );
-  
+
   this.dispatchEvent(event);
 }
