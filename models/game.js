b4d0615235cd6950d2a4a29d34d12b497cb1236e
Gouthamve <cs14btech11014@iith.ac.in>
2015-06-26 20:15:15 +0530

    Model Cleanup Add user as Relation and remove virtual

--- models/game.js
+++ models/game.js
@@ -1,20 +1,16 @@
 var mongoose = require('mongoose');
 
 var gameSchema = mongoose.Schema({
-	user: String,
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	},
 	turns: Number,
 	status: String,
 	StartTime: Date,
 	EndTime: Date,
-	number: Number
+	number: Number,
+	time: Number
 });
-
-gameSchema.virtual('playTime').get(function() {
-	if (this.EndTime) {
-		return ((this.EndTime.getTime() - this.StartTime.getTime()) / 1000)
-	} else {
-		return 0
-	}
-})
 
 module.exports = mongoose.model('Game', gameSchema);
